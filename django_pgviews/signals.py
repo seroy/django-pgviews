2d8568eff7fc1d1e45523a42dbedf97dd3d4cf04
seroy <seroy@bk.ru>
2019-06-06 16:55:04 +0300

    add support the database alias used for synchronization

--- django_pgviews/signals.py
+++ django_pgviews/signals.py
@@ -2,5 +2,5 @@ from django.dispatch import Signal
 
 
 view_synced = Signal(
-    providing_args=['update', 'force', 'status', 'has_changed'])
+    providing_args=['update', 'force', 'status', 'has_changed', 'using'])
 all_views_synced = Signal()
